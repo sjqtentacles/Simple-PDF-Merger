9ccdb8f5394458acdd69ae553f34faa2ec9dd2dd
Sarkisyan <azsarki@USBUR12WB162522s-MacBook-Pro.local>
2020-05-06 22:21:17 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# PDF Merger
+
+Built using PyPDF2 for pdf manipulation, and PySimpleGUI for the GUI.
+Sadly it's TKinter so it looks like shit, but it's pretty speedy, and super simple.
+
+![](screen.png)
+
+It takes in a directory, sorts all the files ending in `.pdf`, and merges them in that order. It writes to the output of the python program as `merged-files.pdf`.
---
+++ main.py
@@ -0,0 +1,34 @@
+from PyPDF2 import PdfFileMerger
+import PySimpleGUI as sg
+import os
+
+def merge_files_in(dir):
+  merger = PdfFileMerger()
+
+  files_in_dir = list(map(lambda x: '{}/{}'.format(dir, x), os.listdir(dir)))
+  pdf_files = list(filter(lambda x: x.endswith('.pdf'), files_in_dir))
+  sorted_pdfs = sorted(pdf_files)
+  
+  for f in sorted_pdfs:
+    input_doc = open(f,'rb')
+    merger.append(input_doc)
+  
+  output = open('merged-files.pdf', 'wb')
+  merger.write(output)
+
+sg.theme('LightPurple')
+layout = [  [sg.Text('Merge Files in Directory in Alphanumerical Order')],
+            [sg.Input(), sg.FolderBrowse()],
+            [sg.Button('Merge'), sg.Exit()] ]
+
+window = sg.Window('PDF Merger', layout)
+while True:
+  event, values = window.read()
+  if event in (None, 'Exit'):
+    break
+  if event == 'Merge':
+    print(values[0])
+    merge_files_in(values[0])
+    sg.popup("Done! Check this program's current folder for the output.")
+
+window.close()
